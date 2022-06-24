9fc55bf2692776ab722348fd365f5a8ecb23a8cf
Joyrudra Biswas <joyrudrabws07@gmail.com>
2022-06-24 18:42:54 +0530

    1st Commi

---
+++ src/com/learningspring/springdemo/BaseballCoach.java
@@ -0,0 +1,7 @@
+package com.learningspring.springdemo;
+
+public class BaseballCoach {
+    public String getDailyWorkout(){
+        return "Train for at least 30 Fuckin Minutes";
+    }
+}
---
+++ src/com/learningspring/springdemo/MyApp.java
@@ -0,0 +1,8 @@
+package com.learningspring.springdemo;
+
+public class MyApp {
+    public static void main(String[] args) {
+        BaseballCoach coach = new BaseballCoach();
+        System.out.println(coach.getDailyWorkout());
+    }
+}
